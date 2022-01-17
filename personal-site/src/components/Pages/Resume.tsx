cd159435c51c421d954ec8e9268df6fcf480b41b
Jamie Thompson <jamie.thompson.1024@gmail.com>
2022-01-17 14:59:38 +1300

    added resume page

---
+++ personal-site/src/components/Pages/Resume.tsx
@@ -0,0 +1,14 @@
+
+import './Resume.css';
+
+function Resume()
+{
+    return (
+        <div className="Resume">
+            <iframe src="/Assets/Resume.pdf">
+            </iframe>
+        </div>
+    )
+}
+
+export default Resume;
