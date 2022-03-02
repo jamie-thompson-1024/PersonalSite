ccd505bcd1f0f3469c1fbb25546ab1cfcd17a216
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-03-02 20:56:06 +1300

    Fixed default project image not showing

--- src/components/Pages/Projects/ProjectItem.tsx
+++ src/components/Pages/Projects/ProjectItem.tsx
@@ -1,19 +1,21 @@
 
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { ProjectInfo } from './ProjectInfo';
 import './ProjectItem.css';
 
 function ProjectItem(props: { projectInfo: ProjectInfo })
 {
-    const onImageNotFound = useCallback((e) => {
-        e.target.src = "/Assets/icons/no-image.svg";
-    }, []);
+    const [ imgSrc, setImgSrc ] = useState(props.projectInfo.thumbnail ?? "/Assets/icons/no-image.svg");
+
+    const onImageNotFound = useCallback(() => {
+        setImgSrc("/Assets/icons/no-image.svg");
+    }, [setImgSrc]);
 
     return (
         <article className="ProjectItem">
             <img 
-                className="ProjectItem-Image"
-                src={ props.projectInfo.thumbnail } 
+                className="ProjectItem-Image" 
+                src={ imgSrc } 
                 alt={ props.projectInfo.name + " Thumbnail" } 
                 onError={onImageNotFound}/>
             <h2 className="ProjectItem-Title">
