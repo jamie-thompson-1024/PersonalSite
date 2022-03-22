0c6595ca4eefc202ce53a1234cf1d7114756695f
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-03-22 15:00:58 +1300

    Fixed nav to top of screen, Added app component overflow handling

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -1,6 +1,6 @@
 
 import SVG from 'react-inlinesvg';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Nav.css';
 
@@ -22,6 +22,9 @@ function Nav()
     const [transition, setTransition] = useState(true);
     const [minTransition, setMinTransistion] = useState(true);
 
+    const navBarRef = useRef<HTMLDivElement>(null);
+    const navBarDisplacerRef = useRef<HTMLDivElement>(null);
+
     const location = useLocation();
 
     const resize = useCallback(() => {
@@ -36,57 +39,83 @@ function Nav()
         };
     }, [resize]);
 
+    // make nav bar displacer element match nav bar height
+    // to offset page content
+    useLayoutEffect(() => {
+        let navBar = navBarRef.current;
+        let navBarDisplacer = navBarDisplacerRef.current;
+        let timeout: any;
+            
+        timeout = setTimeout(() => {
+            if(navBar && navBarDisplacer)
+                navBarDisplacer.style.minHeight = `${navBar.offsetHeight}px`;
+        }, 30);
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    });
+
     if(isMobile)
     {
         if(min)
         {
             return(
-                <nav className={`Nav Nav-Portrait-min ${ minTransition ? "Nav-expand-x-anim" : "Nav-contract-x-anim"}`}>
-                    <button onClick={() => { 
-                            setTransition(true); 
-                            setMinTransistion(false);
-                            setTimeout(() => {
-                                setMin(false);}, 
-                            350); }}>
-                        <SVG src="/Assets/icons/hamburger-menu.svg" />
-                    </button>
-                    <div>{ links.find(link => location.pathname === link.path)?.name }</div>
-                </nav>
+                <>
+                    <nav ref={navBarRef} className={`Nav Nav-Portrait-min ${ minTransition ? "Nav-expand-x-anim" : "Nav-contract-x-anim"}`}>
+                        <button onClick={() => { 
+                                setTransition(true); 
+                                setMinTransistion(false);
+                                setTimeout(() => {
+                                    setMin(false);}, 
+                                350); }}>
+                            <SVG src="/Assets/icons/hamburger-menu.svg" />
+                        </button>
+                        <div>{ links.find(link => location.pathname === link.path)?.name }</div>
+                    </nav>
+                    <div ref={navBarDisplacerRef}></div>
+                </>
             )
         }else{
             return(
-                <nav className={`Nav Nav-Portrait ${ transition ? "Nav-expand-y-anim" : "Nav-contract-y-anim"}`}>
-                    { links.map((link, i) => 
-                        <Link 
-                            className={link.path === location.pathname ? "Nav-ThisLocation" : ""} 
-                            to={link.path} 
-                            key={i}>
-                                { link.name }
-                        </Link>
-                    ) }
-                    <button onClick={() => { 
-                            setTransition(false); 
-                            setMinTransistion(true);
-                            setTimeout(() => {
-                                setMin(true);}, 
-                            400); }}>
-                        <SVG src="/Assets/icons/up-arrow.svg" />
-                    </button>
-                </nav>
+                <>
+                    <nav ref={navBarRef} className={`Nav Nav-Portrait ${ transition ? "Nav-expand-y-anim" : "Nav-contract-y-anim"}`}>
+                        { links.map((link, i) => 
+                            <Link 
+                                className={link.path === location.pathname ? "Nav-ThisLocation" : ""} 
+                                to={link.path} 
+                                key={i}>
+                                    { link.name }
+                            </Link>
+                        ) }
+                        <button onClick={() => { 
+                                setTransition(false); 
+                                setMinTransistion(true);
+                                setTimeout(() => {
+                                    setMin(true);}, 
+                                400); }}>
+                            <SVG src="/Assets/icons/up-arrow.svg" />
+                        </button>
+                    </nav>
+                    <div ref={navBarDisplacerRef}></div>
+                </>
             )
         }
     }else{
         return(
-            <nav className="Nav Nav-Landscape">
-                { links.map((link, i) => 
-                        <Link 
-                            className={link.path === location.pathname ? "Nav-ThisLocation" : ""} 
-                            to={link.path} 
-                            key={i}>
-                                { link.name }
-                        </Link>
-                ) }
-            </nav>
+            <>
+                <nav ref={navBarRef} className="Nav Nav-Landscape">
+                    { links.map((link, i) => 
+                            <Link 
+                                className={link.path === location.pathname ? "Nav-ThisLocation" : ""} 
+                                to={link.path} 
+                                key={i}>
+                                    { link.name }
+                            </Link>
+                    ) }
+                </nav>
+                <div ref={navBarDisplacerRef}></div>
+            </>
         )
     }
 }
