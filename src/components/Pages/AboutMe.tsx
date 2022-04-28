5ec04b7aee84a995086860ae545b15192f8685fc
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-04-28 18:14:36 +1200

    Updated content

--- src/components/Pages/AboutMe.tsx
+++ src/components/Pages/AboutMe.tsx
@@ -12,7 +12,7 @@ function AboutMe()
                         My name is<br />Jamie Thompson
                     </h1>
                     <p>
-                        I am a mostly self-taught software developer based in Canterbury, NZ. Who is interested in 
+                        I am a mostly self-taught software developer based outside of Christchurch, NZ. Who is interested in 
                         furthering my knowledge and experience.
                     </p>
                 </div>
@@ -24,14 +24,14 @@ function AboutMe()
                     programming. I spent the next few years casually tinkering untill 
                     ICT classes in high school helped to give me a more solid foundation 
                     that I would build upon in my own time and during tertiary education.
-                    <br /><br/>
+                    <br /><br />
                     Now I am relatively well versed in front-end web development, mostly vanilla and react.
-                    But am fully willing to learn new frameworks and languages to further my knowledge and experice.
+                    But am fully willing to learn new frameworks and languages to further my knowledge and experience.
                     Recently I have also been delving into back-end frameworks, notably Next.js and Django.
                     While im still a bit new to back-end I would like to become more competent with it and 
                     at some point would like to go full-stack.
                     <br />< br />
-                    Through tertiary education I developed some skills in C# and Java. Although not used 
+                    Through tertiary education I have also developed some skills in C# and Java. Although not used 
                     all that often in my own projects, there is still a decent foundation to build off of.
                 </p>
             </div>
